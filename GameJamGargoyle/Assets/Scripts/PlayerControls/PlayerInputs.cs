98a94b92e0a713dc7d817bc3053681f34b75798b
Yonik-Vinegar <Yannick5215@gmail.com>
2024-05-14 18:27:33 +0100

    PlayerControlsFinished
    Should be finished now

--- GameJamGargoyle/Assets/Scripts/PlayerControls/PlayerInputs.cs
+++ GameJamGargoyle/Assets/Scripts/PlayerControls/PlayerInputs.cs
@@ -28,182 +28,90 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
             ""id"": ""e32a3b74-8164-4b90-a3af-2ad10d976f40"",
             ""actions"": [
                 {
-                    ""name"": ""WASD"",
+                    ""name"": ""Movement"",
                     ""type"": ""PassThrough"",
                     ""id"": ""b829b68b-5e24-4d2a-b483-17e633af80fc"",
-                    ""expectedControlType"": ""Vector3"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
-                    ""name"": ""CameraDirections"",
-                    ""type"": ""PassThrough"",
-                    ""id"": ""312c24c9-cf58-42f2-b058-075527f12bbd"",
-                    ""expectedControlType"": ""Vector3"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                }
-            ],
-            ""bindings"": [
-                {
-                    ""name"": ""3D Vector"",
-                    ""id"": ""d797cc76-8bb8-4574-ba49-0b6d9940f521"",
-                    ""path"": ""3DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""WASD"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""Left"",
-                    ""id"": ""3efd14b0-76ac-4e5e-9b44-1aed0039aefe"",
-                    ""path"": ""<Keyboard>/a"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""WASD"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""Right"",
-                    ""id"": ""222649d9-60a5-4949-bd91-368e33557697"",
-                    ""path"": ""<Keyboard>/d"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""WASD"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""75afe5df-5910-4536-a6c8-15d95d381dd6"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""WASD"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""2567e248-230d-4429-b89f-463e91327378"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""WASD"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""6c1fda0f-3b82-4e7b-9689-64b65602f264"",
-                    ""path"": ""<Mouse>/delta"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""CameraDirections"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                }
-            ]
-        },
-        {
-            ""name"": ""CameraLook"",
-            ""id"": ""f9a0c023-9cbf-4078-82ce-211359e0fedc"",
-            ""actions"": [
-                {
-                    ""name"": ""MouseX"",
-                    ""type"": ""Value"",
-                    ""id"": ""5b57ed6a-551a-476c-a0e6-c8056a2ac16a"",
                     ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": true
+                    ""initialStateCheck"": false
                 },
                 {
-                    ""name"": ""MouseY"",
-                    ""type"": ""Value"",
-                    ""id"": ""b1c759f2-7316-4e55-826a-82a35a3186ed"",
-                    ""expectedControlType"": ""Axis"",
+                    ""name"": ""CameraLook"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""a01060fa-fedd-4dfd-9dd4-d4ac4e7bca9d"",
+                    ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": true
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
                 {
-                    ""name"": """",
-                    ""id"": ""06b16ed5-4862-494a-9abe-ee08cc36ac2d"",
-                    ""path"": ""<Mouse>/delta/y"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""MouseY"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
                     ""name"": ""2D Vector"",
-                    ""id"": ""5e65d9b5-59e7-4c94-9916-6901b1107abc"",
+                    ""id"": ""c2baa79c-e47f-45eb-a70b-b2f0d7061092"",
                     ""path"": ""2DVector"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""MouseX"",
+                    ""action"": ""Movement"",
                     ""isComposite"": true,
                     ""isPartOfComposite"": false
                 },
                 {
                     ""name"": ""up"",
-                    ""id"": ""bd3d0ddf-a055-4ab9-bc20-8f4252d5964e"",
-                    ""path"": ""<Keyboard>/anyKey"",
+                    ""id"": ""ffb3ae9a-3f40-4a55-bf75-6a90b1f316e9"",
+                    ""path"": ""<Keyboard>/w"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""MouseX"",
+                    ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
                 },
                 {
                     ""name"": ""down"",
-                    ""id"": ""b2c5ecb1-8e23-4947-b649-7328eb49216e"",
-                    ""path"": """",
+                    ""id"": ""4ce50ce5-22b4-4fbe-85b4-50878d479aac"",
+                    ""path"": ""<Keyboard>/s"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""MouseX"",
+                    ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
                 },
                 {
                     ""name"": ""left"",
-                    ""id"": ""2c61062b-6ff3-472e-bb08-6b920e1c637b"",
-                    ""path"": """",
+                    ""id"": ""f770ed90-ca45-45f2-8003-380235cda845"",
+                    ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""MouseX"",
+                    ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
                 },
                 {
                     ""name"": ""right"",
-                    ""id"": ""06810d8b-08a8-49cb-8866-0edbb8deba29"",
-                    ""path"": """",
+                    ""id"": ""ee8d6743-927b-438c-8801-c1e9cf3a45d7"",
+                    ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""MouseX"",
+                    ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""97b15866-4a5c-4613-b1e2-eff03436ecca"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""CameraLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -212,12 +120,8 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
 }");
         // Player Movement
         m_PlayerMovement = asset.FindActionMap("Player Movement", throwIfNotFound: true);
-        m_PlayerMovement_WASD = m_PlayerMovement.FindAction("WASD", throwIfNotFound: true);
-        m_PlayerMovement_CameraDirections = m_PlayerMovement.FindAction("CameraDirections", throwIfNotFound: true);
-        // CameraLook
-        m_CameraLook = asset.FindActionMap("CameraLook", throwIfNotFound: true);
-        m_CameraLook_MouseX = m_CameraLook.FindAction("MouseX", throwIfNotFound: true);
-        m_CameraLook_MouseY = m_CameraLook.FindAction("MouseY", throwIfNotFound: true);
+        m_PlayerMovement_Movement = m_PlayerMovement.FindAction("Movement", throwIfNotFound: true);
+        m_PlayerMovement_CameraLook = m_PlayerMovement.FindAction("CameraLook", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -279,14 +183,14 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
     // Player Movement
     private readonly InputActionMap m_PlayerMovement;
     private List<IPlayerMovementActions> m_PlayerMovementActionsCallbackInterfaces = new List<IPlayerMovementActions>();
-    private readonly InputAction m_PlayerMovement_WASD;
-    private readonly InputAction m_PlayerMovement_CameraDirections;
+    private readonly InputAction m_PlayerMovement_Movement;
+    private readonly InputAction m_PlayerMovement_CameraLook;
     public struct PlayerMovementActions
     {
         private @PlayerInputs m_Wrapper;
         public PlayerMovementActions(@PlayerInputs wrapper) { m_Wrapper = wrapper; }
-        public InputAction @WASD => m_Wrapper.m_PlayerMovement_WASD;
-        public InputAction @CameraDirections => m_Wrapper.m_PlayerMovement_CameraDirections;
+        public InputAction @Movement => m_Wrapper.m_PlayerMovement_Movement;
+        public InputAction @CameraLook => m_Wrapper.m_PlayerMovement_CameraLook;
         public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -296,22 +200,22 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
         {
             if (instance == null || m_Wrapper.m_PlayerMovementActionsCallbackInterfaces.Contains(instance)) return;
             m_Wrapper.m_PlayerMovementActionsCallbackInterfaces.Add(instance);
-            @WASD.started += instance.OnWASD;
-            @WASD.performed += instance.OnWASD;
-            @WASD.canceled += instance.OnWASD;
-            @CameraDirections.started += instance.OnCameraDirections;
-            @CameraDirections.performed += instance.OnCameraDirections;
-            @CameraDirections.canceled += instance.OnCameraDirections;
+            @Movement.started += instance.OnMovement;
+            @Movement.performed += instance.OnMovement;
+            @Movement.canceled += instance.OnMovement;
+            @CameraLook.started += instance.OnCameraLook;
+            @CameraLook.performed += instance.OnCameraLook;
+            @CameraLook.canceled += instance.OnCameraLook;
         }
 
         private void UnregisterCallbacks(IPlayerMovementActions instance)
         {
-            @WASD.started -= instance.OnWASD;
-            @WASD.performed -= instance.OnWASD;
-            @WASD.canceled -= instance.OnWASD;
-            @CameraDirections.started -= instance.OnCameraDirections;
-            @CameraDirections.performed -= instance.OnCameraDirections;
-            @CameraDirections.canceled -= instance.OnCameraDirections;
+            @Movement.started -= instance.OnMovement;
+            @Movement.performed -= instance.OnMovement;
+            @Movement.canceled -= instance.OnMovement;
+            @CameraLook.started -= instance.OnCameraLook;
+            @CameraLook.performed -= instance.OnCameraLook;
+            @CameraLook.canceled -= instance.OnCameraLook;
         }
 
         public void RemoveCallbacks(IPlayerMovementActions instance)
@@ -329,68 +233,9 @@ public partial class @PlayerInputs: IInputActionCollection2, IDisposable
         }
     }
     public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
-
-    // CameraLook
-    private readonly InputActionMap m_CameraLook;
-    private List<ICameraLookActions> m_CameraLookActionsCallbackInterfaces = new List<ICameraLookActions>();
-    private readonly InputAction m_CameraLook_MouseX;
-    private readonly InputAction m_CameraLook_MouseY;
-    public struct CameraLookActions
-    {
-        private @PlayerInputs m_Wrapper;
-        public CameraLookActions(@PlayerInputs wrapper) { m_Wrapper = wrapper; }
-        public InputAction @MouseX => m_Wrapper.m_CameraLook_MouseX;
-        public InputAction @MouseY => m_Wrapper.m_CameraLook_MouseY;
-        public InputActionMap Get() { return m_Wrapper.m_CameraLook; }
-        public void Enable() { Get().Enable(); }
-        public void Disable() { Get().Disable(); }
-        public bool enabled => Get().enabled;
-        public static implicit operator InputActionMap(CameraLookActions set) { return set.Get(); }
-        public void AddCallbacks(ICameraLookActions instance)
-        {
-            if (instance == null || m_Wrapper.m_CameraLookActionsCallbackInterfaces.Contains(instance)) return;
-            m_Wrapper.m_CameraLookActionsCallbackInterfaces.Add(instance);
-            @MouseX.started += instance.OnMouseX;
-            @MouseX.performed += instance.OnMouseX;
-            @MouseX.canceled += instance.OnMouseX;
-            @MouseY.started += instance.OnMouseY;
-            @MouseY.performed += instance.OnMouseY;
-            @MouseY.canceled += instance.OnMouseY;
-        }
-
-        private void UnregisterCallbacks(ICameraLookActions instance)
-        {
-            @MouseX.started -= instance.OnMouseX;
-            @MouseX.performed -= instance.OnMouseX;
-            @MouseX.canceled -= instance.OnMouseX;
-            @MouseY.started -= instance.OnMouseY;
-            @MouseY.performed -= instance.OnMouseY;
-            @MouseY.canceled -= instance.OnMouseY;
-        }
-
-        public void RemoveCallbacks(ICameraLookActions instance)
-        {
-            if (m_Wrapper.m_CameraLookActionsCallbackInterfaces.Remove(instance))
-                UnregisterCallbacks(instance);
-        }
-
-        public void SetCallbacks(ICameraLookActions instance)
-        {
-            foreach (var item in m_Wrapper.m_CameraLookActionsCallbackInterfaces)
-                UnregisterCallbacks(item);
-            m_Wrapper.m_CameraLookActionsCallbackInterfaces.Clear();
-            AddCallbacks(instance);
-        }
-    }
-    public CameraLookActions @CameraLook => new CameraLookActions(this);
     public interface IPlayerMovementActions
     {
-        void OnWASD(InputAction.CallbackContext context);
-        void OnCameraDirections(InputAction.CallbackContext context);
-    }
-    public interface ICameraLookActions
-    {
-        void OnMouseX(InputAction.CallbackContext context);
-        void OnMouseY(InputAction.CallbackContext context);
+        void OnMovement(InputAction.CallbackContext context);
+        void OnCameraLook(InputAction.CallbackContext context);
     }
 }
