ac672a08e04215c1efacc9e7c143877278ebcebe
a_person <alexanderbeaumont05@hotmail.com>
2024-05-16 05:13:21 +0100

    removed bloat and added ambience
    ambient sounds are in!
    also removed pointless demo scenes.

--- GameJamGargoyle/Assets/Scripts/SoundController.cs
+++ GameJamGargoyle/Assets/Scripts/SoundController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<AudioClip> audioClips;
+    public AudioClip currentClip;
+    public AudioSource source;
+    public float minWaitBetweenPlays = 1f;
+    public float maxWaitBetweenPlays = 5f;
+    public float waitTimeCountdown = -1f;
+    public GameObject player;
+ 
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        if (!source.isPlaying)
+        {
+            if (waitTimeCountdown < 0f)
+            {
+                currentClip = audioClips[Random.Range(0, audioClips.Count)];
+                source.clip = currentClip;
+                source.Play();
+                waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
+                transform.position = player.transform.position + new Vector3(Random.Range(-10,10),0,Random.Range(-10,10));
+            }
+            else
+            {
+                waitTimeCountdown -= Time.deltaTime;
+            }
+        }
     }
 }
